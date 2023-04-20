668a8bd6e80781fde8886e2a249229788b4f3b37
Avishowman <72096062+Avishowman@users.noreply.github.com>
2023-04-20 14:28:32 +0530

    Update index.js
    add keypress feature

--- index.js
+++ index.js
@@ -41,5 +41,44 @@ function handleClick() {
  
 }
 
+document.addEventListener("keypress",handleKeypress)
+
+function handleKeypress(event){
+  var wordkeyPress = event.key;
+  // console.log(wordkeyPress)
+  switch (wordkeyPress) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+
+    default:
+      break;
+  }
+}
 // var audio = new Audio('sounds/tom-1.mp3');
 //     audio.play();
